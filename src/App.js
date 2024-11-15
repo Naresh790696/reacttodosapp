9da595c42ea305c20500c342aca819ac30d1becc
narendra <nilesh9012345456@gmail.com>
2024-11-15 10:12:45 +0530

    form validation functionality

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+// Importing Components;
+import Form from './componets/form';
+
+const App = () => {
+  return (
+   <>
+   <h1 className='fw-bolder fst-italic fs-1 text-center mt-3 mb-0' style={{color:"#2874f0"}}>Form validation</h1>
+   <Form/>
+   </>
+  )
+}
+
+export default App;
