16aac8ec3532a4bb1ab7997c639ca28dbf562192
narendra <nilesh9012345456@gmail.com>
2024-11-15 11:34:39 +0530

    Speell

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Form from './componets/form';
 const App = () => {
   return (
    <>
-   <h1 className='fw-bolder fst-italic fs-1 text-center mt-3 mb-0' style={{color:"#2874f0"}}>Form validation</h1>
+   <h1 className='fw-bolder fst-italic fs-1 text-center mt-3 mb-0' style={{color:"#2874f0"}}>Form Validation</h1>
    <Form/>
    </>
   )
