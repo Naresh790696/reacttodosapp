580abd7b10b2b7faa797d330b5aabaf4b3fb3456
narendra <nilesh9012345456@gmail.com>
2024-11-15 14:41:37 +0530

    heading tag change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Form from './componets/form';
 const App = () => {
   return (
    <>
-   <h1 className='fw-bolder fst-italic fs-1 text-center mt-3 mb-0' style={{color:"#2874f0"}}>Form Validation</h1>
+   <h3 className='fw-bolder fst-italic fs-1 text-center mt-3 mb-0' style={{color:"#2874f0"}}>Form Validation</h3>
    <Form/>
    </>
   )
